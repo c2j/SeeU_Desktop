4b0fc22d7fb30ca0e3bc3559206732483438918c
xmingtec <liudm@xming-tec.com>
2025-06-23 13:36:59 +0800

    build in aarh64 l429

--- scripts/build-l420.sh
+++ scripts/build-l420.sh
@@ -3,7 +3,11 @@
 
 cd ..
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-docker build -t seeu-desktop-linux-builder -f Dockerfile.l420 .
-docker create --name seeu-temp-container seeu-desktop-linux-builder
+
+#docker build -t seeu-desktop-linux-builder-arm64v8-base -f Dockerfile.arm64.base .
+
+docker build -t seeu-desktop-linux-builder-arm64v8 -f Dockerfile.l420 .
+docker create --name seeu-temp-container seeu-desktop-linux-builder-arm64v8
 docker cp seeu-temp-container:/output/seeu_desktop target/arm64v8/
 docker rm seeu-temp-container
+docker image rm seeu-desktop-linux-builder-arm64v8
