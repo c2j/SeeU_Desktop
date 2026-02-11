e81dc90e4249d3857945fa2696a8813a475e2cc6
Jianjun Chen <chenjj.yz@gmail.com>
2026-02-12 00:09:21 +0800

    fix docker build issue on arm64 mac.

--- scripts/build-l420.sh
+++ scripts/build-l420.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Build script for Linux targets using vcpkg with fallback support
 
-cd ..
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+#cd ..
+#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
 #docker build -t seeu-desktop-linux-builder-arm64v8-base -f Dockerfile.arm64.base .
 
-docker build -t seeu-desktop-linux-builder-arm64v8 -f Dockerfile.l420 .
+DOCKER_BUILDKIT=1 docker build --provenance=false -t seeu-desktop-linux-builder-arm64v8 -f Dockerfile.l420 .
 docker create --name seeu-temp-container seeu-desktop-linux-builder-arm64v8
+mkdir -p target/arm64v8
 docker cp seeu-temp-container:/output/seeu_desktop target/arm64v8/
 docker rm seeu-temp-container
 docker image rm seeu-desktop-linux-builder-arm64v8
